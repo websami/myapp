916340ea448eb468dfbf2134260bb757e30b24f0
sambs <samsaravillo@gmail.com>
2018-03-25 15:49:11 +0800

    updated email and password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,19 +11,19 @@ export class LoginComponent implements OnInit {
     username: string = '';
     password: string = '';
     return: string = '';
-    
+
   constructor(private router:Router, private email:UserService) { }
 
   ngOnInit() {
   }
-    
+
     loginUser(e) {
         e.preventDefault();
         console.log(e);
         var email =  e.target.elements[0].value;
         var password = e.target.elements[1].value;
-        
-        if(email == '' && password == ''){ //test@nv.com tested
+
+        if(email == 'test@nv.com' && password == 'tested'){ //test@nv.com tested
             this.email.setUserLoggedIn();
             this.router.navigate(['dashboard']);
             console.log("trueasdf");
