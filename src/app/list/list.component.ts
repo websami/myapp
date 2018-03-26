d0179527b67bb5e117ae0c0a363ea37865c047e7
sambs <samsaravillo@gmail.com>
2018-03-26 22:58:40 +0800

    App compiled
    App compiled

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,24 +3,27 @@ import { User } from '../user';
 import { USERS } from '../mock-users';
 import { FilterPipe } from '../filter.pipe';
 
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-    users = USERS; 
-    
+    users = USERS;
+
     selectedUser: User;
-    
+
+    search = '';
+
   constructor() { }
 
   ngOnInit() {
   }
-    
+
     onSelect(user: User): void {
         this.selectedUser = user;
     }
-    
+
 
 }
--- src/app/mock-users.ts
+++ src/app/mock-users.ts
@@ -1,14 +1,14 @@
 import { User } from './user';
 
 export const USERS: User[] = [
-    { id: 1, img: 'icon-image-40', firstName: 'Firstname', lastName: 'lastname', phoneNumber: 12334, birthDate: '1987-05-07', country: 'country', unComplete: 'Complete' },
-    { id: 2, img: 'icon-image-40', firstName: 'Firstname2', lastName: 'lastname', phoneNumber: null, birthDate: '1982-03-01', country: 'country', unComplete: 'Complete' },
-    { id: 3, img: 'icon-image-40', firstName: 'Firstname3', lastName: 'lastname', phoneNumber: null, birthDate: '1985-11-14', country: 'country', unComplete: 'Complete' },
-    { id: 4, img: 'icon-image-40', firstName: 'Firstname4', lastName: 'lastname', phoneNumber: null, birthDate: '1986-12-21', country: 'country', unComplete: 'Complete' },
-    { id: 5, img: 'icon-image-40', firstName: 'Firstname5', lastName: 'lastname', phoneNumber: null, birthDate: '1988-08-01', country: 'country', unComplete: 'Complete' },
-    { id: 6, img: 'icon-image-40', firstName: 'Firstname6', lastName: 'lastname', phoneNumber: null, birthDate: '1987-08-10', country: 'country', unComplete: 'Complete' },
-    { id: 7, img: 'icon-image-40', firstName: 'Firstname7', lastName: 'lastname', phoneNumber: null, birthDate: '1999-12-25', country: 'country', unComplete: 'Complete' },
-    { id: 8, img: 'icon-image-40', firstName: 'Firstname8', lastName: 'lastname', phoneNumber: null, birthDate: '1979-09-02', country: 'country', unComplete: 'Complete' },
-    { id: 9, img: 'icon-image-40', firstName: 'Firstname9', lastName: 'lastname', phoneNumber: null, birthDate: '1981-05-03', country: 'country', unComplete: 'Complete' },
-    { id: 10, img: 'icon-image-40', firstName: 'Firstname10', lastName: 'lastname', phoneNumber: null, birthDate: '1989-03-05', country: 'country', unComplete: 'Complete' }
+    { id: 1, img: 'icon-image-40', firstName: 'John', lastName: 'lastname', phoneNumber: 12334, birthDate: '1987-05-07', country: 'country', unComplete: 'Complete' },
+    { id: 2, img: 'icon-image-40', firstName: 'Dave', lastName: 'lastname', phoneNumber: null, birthDate: '1982-03-01', country: 'country', unComplete: 'Complete' },
+    { id: 3, img: 'icon-image-40', firstName: 'Smith', lastName: 'lastname', phoneNumber: null, birthDate: '1985-11-14', country: 'country', unComplete: 'Complete' },
+    { id: 4, img: 'icon-image-40', firstName: 'Katrina', lastName: 'lastname', phoneNumber: null, birthDate: '1986-12-21', country: 'country', unComplete: 'Complete' },
+    { id: 5, img: 'icon-image-40', firstName: 'Sammy', lastName: 'lastname', phoneNumber: null, birthDate: '1988-08-01', country: 'country', unComplete: 'Complete' },
+    { id: 6, img: 'icon-image-40', firstName: 'George', lastName: 'lastname', phoneNumber: null, birthDate: '1987-08-10', country: 'country', unComplete: 'Complete' },
+    { id: 7, img: 'icon-image-40', firstName: 'Anna', lastName: 'lastname', phoneNumber: null, birthDate: '1999-12-25', country: 'country', unComplete: 'Complete' },
+    { id: 8, img: 'icon-image-40', firstName: 'Mae', lastName: 'lastname', phoneNumber: null, birthDate: '1979-09-02', country: 'country', unComplete: 'Complete' },
+    { id: 9, img: 'icon-image-40', firstName: 'Steve', lastName: 'lastname', phoneNumber: null, birthDate: '1981-05-03', country: 'country', unComplete: 'Complete' },
+    { id: 10, img: 'icon-image-40', firstName: 'Junna', lastName: 'lastname', phoneNumber: null, birthDate: '1989-03-05', country: 'country', unComplete: 'Complete' }
 ];
